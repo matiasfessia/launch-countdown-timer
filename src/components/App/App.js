54099edb8c7629879549326d22771742c8cbc500
Matias Fessia <matiasfessia@MacBook-Pro-de-Matias.local>
2021-04-05 08:07:40 -0300

    Clean up root component app.

---
+++ src/components/App/App.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+export const App = () => {
+  return <div className="app"></div>;
+};
+
+export default App;
---
+++ src/components/App/App.test.js
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import App from "./App";
+
+it("renders correctly", () => {
+  const { container } = render(<App />);
+  expect(container).toMatchSnapshot();
+});
